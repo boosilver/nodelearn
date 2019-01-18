0f0f8ed96cd2da237f6e26200f784e9f046b57f9
saharat sattee <saharatsatteee@Rush-Saharat-2.local>
2019-01-18 11:08:15 +0700

    rush

--- go/server.go
+++ go/server.go
@@ -1,64 +1,98 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
+	"github.com/labstack/echo"
 	"time"
-
-	"github.com/gorilla/mux"
+	"strconv"
+	"fmt"
 )
 
-type Todo struct {
-	Name      string    `json:"name"`
-	Completed bool      `json:"completed"`
-	Due       time.Time `json:"due"`
-}
-
-type Todos []Todo
-
 func main() {
-
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-	router.HandleFunc("/todos/{todoId}", TodoShow)
-	router.HandleFunc("/sum", sum)
-
-	log.Fatal(http.ListenAndServe(":1234", router))
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World! i am go ")
+	})
+	e.GET("/serverdate", getServerTime)
+	//e.POST("/sumsum",AmodSUM)
+	e.GET("/sumsum/:a",gettypetime)
+	e.Logger.Fatal(e.Start(":1234"))
+	//e.GET("/testtype", testtype) //int to string ได้แล้วววววววววววววววววว
+	//e.GET("/testtype", testtype)  //string to int ได้สักทีโว้ยยยย
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome!")
+func getServerTime(c echo.Context) error {
+	t := time.Now().Format("2006-01-02 03:04:05")
+	return c.String(http.StatusOK, "Time now : " + t)
 }
+//func testtype(c echo.Context) error {
+	//var i int = 12345 ;
+	//strInt := strconv.Itoa(i)
+	//return c.String(http.StatusOK, "Time now : " + strInt)   //int เป็น string
+//}
 
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
+//func testtype(c echo.Context) error {
+	//str  :=  "1";
+	//intI, err := strconv.Atoi (str)
+	//if err == nil{
+	//	fmt.Println(intI)
+	//}else{
+	//	fmt.Println(err)
+	//}
+	//intI = intI+5
+	//fmt.Println(intI)
+	//strInt := strconv.Itoa(intI)
+	//return c.String(http.StatusOK, "Time now : " + strInt)   // string to int ได้แล้ววว
+//}
 
-	json.NewEncoder(w).Encode(todos)
-}
 
-// func TodoIndex(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "Todo Index!")
+func gettypetime(c echo.Context) error {
+	var yy,mm,dd,aa = 0.0,0.0,0.0,0.0 ;
+
+	a := c.Param("a")
+	 aa, err := strconv.ParseFloat(a, 64)
+	if err == nil {
+		fmt.Println(aa) }
+
+	y := time.Now().Format("2006")
+	 yy, err = strconv.ParseFloat(y, 64)
+	if err == nil {
+		fmt.Println(yy) }
+
+	m := time.Now().Format("01")
+	mm, err = strconv.ParseFloat(m, 64) 
+	if err == nil {
+		fmt.Println(mm) }
+
+	d := time.Now().Format("02")
+	dd, err = strconv.ParseFloat(d, 64) 
+	if err == nil {
+		fmt.Println(dd) }
+	sum := yy+mm+dd;
+	suma := sum/aa;
+	r := fmt.Sprintf("%.4f", suma)
+
+	return c.String(http.StatusOK, "sumsum : " + r) 
+
+
+
+
+	
+	//yy:= strconv.ParseInt(y)
+	//mm:= strconv.ParseInt(m)
+	//yy, err := strconv.Atoi(y)
+	//a := yy+mm
+	
+	//return y,m,d
+	//strInt := strconv.Itoa(err)
+	//return c.String(http.StatusOK,"a is "+a )
+ }
+
+
+
+// func AmodSUM(c echo.Context) error {
+
+	//s := fmt.Sprintf("%.2f", 12.3456) // s == "12.35" แปลงทศนิยม
+
+
 // }
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoId"]
-	fmt.Fprintln(w, "Todo show:", todoId)
-}
-
-func sum(w http.ResponseWriter, r *http.Request) {
-	t := time.Now()
-	y := float32(t.Year())
-	mon := t.Month()
-	var i int = int(mon)
-	var g = float32(i)
-	d := float32(t.Day())
-	sum := y + g + d
-	fmt.Fprintln(sum)
-}
